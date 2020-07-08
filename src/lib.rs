7e1bb0fb598136128a66ecb3184a38e852025fd8
Tyler Hill <tyhi@tyhi.rs>
2020-07-08 08:51:40 -0500

    Refactor & changed encode signature
    * Using num-traits to allow any number as id
    * Refactored to reduce LOC
    
    Signed-off-by: Tyler Hill <tyhi@tyhi.rs>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use num_traits::{Num, NumCast};
+
 pub static DEFAULT_ALPHABET: &str = "mn6j2c4rv8bpygw95z7hsdaetxuk3fq";
 pub static DEFAULT_BLOCK_SIZE: usize = 24;
 pub static MIN_LENGTH: usize = 5;
@@ -13,30 +15,34 @@ impl UrlEncoder {
         Self {
             alphabet: alp.chars().collect(),
             mask: (1 << block) - 1,
-            mapping: (0..block).map(usize::from).collect(),
+            mapping: (0..block).map(std::convert::From::from).collect(),
         }
     }
 
-    pub fn encode_url(&self, n: usize, min_length: usize) -> String {
-        return self.enbase(self.encode(n), min_length);
+    pub fn encode_url<N: Num + NumCast>(&self, n: N, min_length: usize) -> String {
+        return self.enbase(
+            self.encode(NumCast::from(n).expect("number was unable to bet converted to usize")),
+            min_length,
+        );
     }
 
-    pub fn decode_url(&self, x: String) -> usize {
-        self.decode(self.debase(x))
+    pub fn decode_url(&self, x: String) -> Option<usize> {
+        Some(self.decode(self.debase(x)?))
     }
+
     fn encode(&self, n: usize) -> usize {
         n & !self.mask | self._encode(n & self.mask)
     }
 
     fn _encode(&self, n: usize) -> usize {
         let mut result = 0;
-        for (i, b) in self.mapping.iter().rev().enumerate() {
+
+        self.mapping.iter().rev().enumerate().for_each(|(i, b)| {
             if n & (1 << i) != 0 {
                 result |= 1 << b;
-            } else {
-                continue;
             }
-        }
+        });
+
         result
     }
 
@@ -46,21 +52,22 @@ impl UrlEncoder {
 
     fn _decode(&self, n: usize) -> usize {
         let mut result = 0;
-        for (i, b) in self.mapping.iter().rev().enumerate() {
+
+        self.mapping.iter().rev().enumerate().for_each(|(i, b)| {
             if n & (1 << b) != 0 {
                 result |= 1 << i;
             }
-        }
+        });
+
         result
     }
 
     fn enbase(&self, x: usize, min_length: usize) -> String {
         let result = self._enbase(x);
 
-        let pad_num = if min_length < result.chars().count() {
-            0
-        } else {
-            min_length - result.chars().count()
+        let pad_num = match min_length < result.chars().count() {
+            true => 0,
+            false => min_length - result.chars().count(),
         };
 
         let padding = self.alphabet[0].to_string().repeat(pad_num);
@@ -70,31 +77,32 @@ impl UrlEncoder {
 
     fn _enbase(&self, x: usize) -> String {
         let n = self.alphabet.len();
-        if x < n as usize {
-            return vec![self.alphabet[x as usize]].into_iter().collect();
+        if x < n {
+            return self.alphabet[x].to_string();
         }
 
-        let mut rt: Vec<char> = vec![];
+        let mut rt: Vec<char> = Vec::new();
         let mut xx = x;
-        loop {
-            if xx < n {
-                rt.insert(0, self.alphabet[xx]);
-                break;
-            }
+
+        while !(xx < n) {
             rt.insert(0, self.alphabet[xx % n]);
             xx /= n;
         }
 
+        rt.insert(0, self.alphabet[xx]);
+
         rt.into_iter().collect()
     }
 
-    fn debase(&self, x: String) -> usize {
+    fn debase(&self, x: String) -> Option<usize> {
         let n = self.alphabet.len();
         let mut result = 0;
-        for (i, c) in x.chars().into_iter().rev().enumerate() {
+
+        x.chars().into_iter().rev().enumerate().for_each(|(i, c)| {
             result += self.alphabet.iter().position(|&x| x == c).unwrap() * (n.pow(i as u32))
-        }
-        result
+        });
+
+        Some(result)
     }
 }
 
@@ -119,6 +127,6 @@ mod tests {
             super::DEFAULT_BLOCK_SIZE,
         );
 
-        assert_eq!(1, e.decode_url("867nv".into()));
+        assert_eq!(1, e.decode_url("867nv".into()).unwrap());
     }
 }
